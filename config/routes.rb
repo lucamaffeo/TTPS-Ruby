950ce59bb01f011cbb48044c658535e0a48cddae
franco <francobof01@gmail.com>
2025-11-22 16:24:06 -0300

    añado rutas del store front

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
 
   # Root: formulario de login de Devise para Usuario
   devise_scope :usuario do
-    root to: "devise/sessions#new"
+    root to: "devise/sessions#new"  
+  end
+
+  # STORE FRONT (vista pública)
+  namespace :storefront do
+    root "productos#index"            # GET /storefront
+    resources :productos, only: [:index, :show]
   end
 end
